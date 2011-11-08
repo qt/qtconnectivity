9d4f7d78a622591ca4e4f6b467c2793e772b83ba
Michael Zanetti <michael.zanetti@nokia.com>
2011-11-08 17:05:35 +0100

    ported bluetooth and nfc declarative plugins from QtMobility to QtConnectivity
    Change-Id: I1792d516ca80418af9fc6249e4aa55964fc94b38

---
+++ src/imports/nfc/qdeclarativendeffilter_p.h
@@ -0,0 +1,78 @@
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the QtNfc module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef QDECLARATIVENDEFFILTER_P_H
+#define QDECLARATIVENDEFFILTER_P_H
+
+#include <QtCore/QObject>
+
+class QDeclarativeNdefFilter : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QString type READ type WRITE setType NOTIFY typeChanged)
+    Q_PROPERTY(int minimum READ minimum WRITE setMinimum NOTIFY minimumChanged)
+    Q_PROPERTY(int maximum READ maximum WRITE setMaximum NOTIFY maximumChanged)
+
+public:
+    explicit QDeclarativeNdefFilter(QObject *parent = 0);
+
+    QString type() const;
+    void setType(const QString &t);
+
+    int minimum() const;
+    void setMinimum(int value);
+
+    int maximum() const;
+    void setMaximum(int value);
+
+signals:
+    void typeChanged();
+    void minimumChanged();
+    void maximumChanged();
+
+private:
+    QString m_type;
+    int m_minimum;
+    int m_maximum;
+};
+
+#endif // QDECLARATIVENDEFFILTER_P_H
