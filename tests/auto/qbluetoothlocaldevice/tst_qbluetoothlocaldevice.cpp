4689aeed7c33b696f430c89d4423febe1fad1f7e
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2012-01-03 14:38:11 +0100

    QtConnectivity: Fix test crashes/asserts/hangs on Windows.
    - Fix crash caused by uninitialized d-ptr in local device
      default implementation.
    - Terminate wait loops
    - Check signal spy before accessing first element.
    - Fix compiler warnings.
    
    Change-Id: I92d54b4dfcc05e8befb49f2bd72b60251641aa48

--- tests/auto/qbluetoothlocaldevice/tst_qbluetoothlocaldevice.cpp
+++ tests/auto/qbluetoothlocaldevice/tst_qbluetoothlocaldevice.cpp
@@ -195,6 +195,7 @@ void tst_QBluetoothLocalDevice::tst_hostModes()
         QTRY_VERIFY(hostModeSpy.count() > 0);
     }
     // test the actual signal values.
+    QVERIFY(hostModeSpy.count() > 0);
     QList<QVariant> arguments = hostModeSpy.takeFirst();
     QBluetoothLocalDevice::HostMode hostMode = qvariant_cast<QBluetoothLocalDevice::HostMode>(arguments.at(0));
     QCOMPARE(hostModeExpected, hostMode);
