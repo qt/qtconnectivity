e4cf274be6730e3fa35ca71ff234292bafb06045
Michael Zanetti <michael.zanetti@nokia.com>
2012-02-24 14:53:47 +0100

    also signal a failed transfer to the Bluetooth Services
    Change-Id: I82e59ca93cd3861354ddacd0fc97388109633751

--- src/bluetooth/qbluetoothtransferreply_bluez.cpp
+++ src/bluetooth/qbluetoothtransferreply_bluez.cpp
@@ -172,6 +172,13 @@ void QBluetoothTransferReplyBluez::sendReturned(QDBusPendingCallWatcher *watcher
         else
             m_error = QBluetoothTransferReply::UnknownError;
 
+#ifdef NOKIA_BT_SERVICES
+        // Inform the service daemon that an outgoing transfer has failed (use a fake transfer ID as there is no real one yet)
+        QFile *file = qobject_cast<QFile *>(source);
+        nokiaBtServiceInstance()->outgoingFile("/transfer9999", address.toString(), file->fileName(), QBluetoothTransferReply::attribute(QBluetoothTransferRequest::TypeAttribute).toString(), m_size);
+        nokiaBtServiceInstance()->setTransferFinished("/transfer9999", false);
+#endif
+
         // allow time for the developer to connect to the signal
         QMetaObject::invokeMethod(this, "finished", Qt::QueuedConnection, Q_ARG(QBluetoothTransferReply*, this));
     }
