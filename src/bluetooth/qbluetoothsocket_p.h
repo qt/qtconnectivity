b7f0fa463b4a95d8b962797a2ac1d2c773e72a18
alex <alex.blasche@nokia.com>
2012-02-27 16:18:39 +0100

    Move qprivatelinearbuffer_p.h into bluetooth project
    In general we don't have any code in MODULE_SRC/src
    
    Change-Id: I412e5de3708a036df8e5a76fcd7870faea90fb5d

--- src/bluetooth/qbluetoothsocket_p.h
+++ src/bluetooth/qbluetoothsocket_p.h
@@ -47,7 +47,7 @@
 #ifndef QPRIVATELINEARBUFFER_BUFFERSIZE
 #define QPRIVATELINEARBUFFER_BUFFERSIZE Q_INT64_C(16384)
 #endif
-#include "../qprivatelinearbuffer_p.h"
+#include "qprivatelinearbuffer_p.h"
 
 #include <QtGlobal>
 
