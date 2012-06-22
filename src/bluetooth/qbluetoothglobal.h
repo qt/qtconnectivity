284d4057cf7bd95cf80ba1334ae66972895fe768
Thiago Macieira <thiago.macieira@intel.com>
2012-06-22 11:01:15 +0200

    Update the export macros in qtconnectivity.git
    Use the new, simpler QT_STATIC macro.
    
    Change-Id: I054286e10bc10ef886b93d9dd58471769f9aed5b

--- src/bluetooth/qbluetoothglobal.h
+++ src/bluetooth/qbluetoothglobal.h
@@ -56,30 +56,14 @@
 #endif
 
 
-#if defined(Q_OS_WIN)
-#  if defined(QT_NODLL)
-#    undef QT_MAKEDLL
-#    undef QT_DLL
-#  elif defined(QT_MAKEDLL)
-#    if defined(QT_DLL)
-#      undef QT_DLL
-#    endif
+#ifndef QT_STATIC
 #    if defined(QT_BUILD_BLUETOOTH_LIB)
 #      define Q_BLUETOOTH_EXPORT Q_DECL_EXPORT
 #    else
 #      define Q_BLUETOOTH_EXPORT Q_DECL_IMPORT
 #    endif
-#  elif defined(QT_DLL)
-#    define Q_BLUETOOTH_EXPORT Q_DECL_IMPORT
-#  endif
-#endif
-
-#if !defined(Q_BLUETOOTH_EXPORT)
-#  if defined(QT_SHARED)
-#    define Q_BLUETOOTH_EXPORT Q_DECL_EXPORT
-#  else
+#else
 #    define Q_BLUETOOTH_EXPORT
-#  endif
 #endif
 
 #endif // QTBLUETOOTH_H
