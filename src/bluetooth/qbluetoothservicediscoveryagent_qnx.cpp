aaada70924dd53dbc9763581bc076ddb1aa8c9c9
Alex Blasche <alexander.blasche@digia.com>
2014-08-27 12:19:11 +0200

    Set ATT as protocol descriptor for BTLE SDP records
    At the same time we fix up some cases where the ProtocolDescriptorList
    was not a list within a list.
    
    Change-Id: If0ec6cf6374902a47a905edd08523906be9fd86b
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>

--- src/bluetooth/qbluetoothservicediscoveryagent_qnx.cpp
+++ src/bluetooth/qbluetoothservicediscoveryagent_qnx.cpp
@@ -278,8 +278,10 @@ void QBluetoothServiceDiscoveryAgentPrivate::remoteDevicesChanged(int fd)
         QBluetoothServiceInfo serviceInfo;
         serviceInfo.setDevice(discoveredDevices.at(0));
 
-        QBluetoothServiceInfo::Sequence  protocolDescriptorList;
-        protocolDescriptorList << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::L2cap));
+        QBluetoothServiceInfo::Sequence protocolDescriptorList;
+        QBluetoothServiceInfo::Sequence l2cpProtocol;
+        l2cpProtocol << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::L2cap));
+        protocolDescriptorList.append(QVariant::fromValue(l2cpProtocol));
 
         bool ok;
         QBluetoothUuid suuid(QByteArray(next_service).toUInt(&ok,16));
@@ -361,7 +363,16 @@ void QBluetoothServiceDiscoveryAgentPrivate::remoteDevicesChanged(int fd)
         lowEnergyService.setAttribute(QBluetoothServiceInfo::ServiceClassIds, classId);
 
         QBluetoothServiceInfo::Sequence protocolDescriptorList;
-        protocolDescriptorList << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::L2cap));
+        {
+            QBluetoothServiceInfo::Sequence protocol;
+            protocol << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::L2cap));
+            protocolDescriptorList.append(QVariant::fromValue(protocol));
+        }
+        {
+            QBluetoothServiceInfo::Sequence protocol;
+            protocol << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::Att));
+            protocolDescriptorList.append(QVariant::fromValue(protocol));
+        }
         service.setAttribute(QBluetoothServiceInfo::ProtocolDescriptorList, protocolDescriptorList);
 
         qCDebug(QT_BT_QNX) << "Adding Low Energy service" << leUuid;
