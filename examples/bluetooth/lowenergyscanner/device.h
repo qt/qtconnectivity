6837d143b456eb1f253b68704a5ff1b48bef4de4
Alex Blasche <alexander.blasche@digia.com>
2014-09-01 13:46:59 +0200

    Add support for random vs public address selection to lowenergyscanner
    Change-Id: I963f47f0089d072249b73bdfb0c715d43186b08d
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>

--- examples/bluetooth/lowenergyscanner/device.h
+++ examples/bluetooth/lowenergyscanner/device.h
@@ -65,6 +65,7 @@ class Device: public QObject
     Q_PROPERTY(QVariant servicesList READ getServices NOTIFY servicesUpdated)
     Q_PROPERTY(QVariant characteristicList READ getCharacteristics NOTIFY characteristicsUpdated)
     Q_PROPERTY(QString update READ getUpdate NOTIFY updateChanged)
+    Q_PROPERTY(bool useRandomAddress READ isRandomAddress WRITE setRandomAddress NOTIFY randomAddressChanged)
     Q_PROPERTY(bool state READ state NOTIFY stateChanged)
 public:
     Device();
@@ -74,6 +75,9 @@ public:
     QVariant getCharacteristics();
     QString getUpdate();
     bool state();
+
+    bool isRandomAddress() const;
+    void setRandomAddress(bool newValue);
 
 public slots:
     void startDeviceDiscovery();
@@ -105,6 +109,7 @@ Q_SIGNALS:
     void updateChanged();
     void stateChanged();
     void disconnected();
+    void randomAddressChanged();
 
 private:
     void setUpdate(QString message);
@@ -117,6 +122,7 @@ private:
     bool connected;
     QLowEnergyController *controller;
     bool m_deviceScanState;
+    bool randomAddress;
 };
 
 #endif // DEVICE_H
