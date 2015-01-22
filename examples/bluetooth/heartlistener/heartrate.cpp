769a597083a60dc530b9a28fe606dae6d1b02369
Timur Pocheptsov <Timur.Pocheptsov@digia.com>
2015-01-22 17:53:20 +0100

    Bluetooth LE - hearlistener example (OS X, iOS)
    Workaround for CoreBluetooth's limitation (no addresses, only 'uuids').
    
    Change-Id: I9ab83a403d4643df11320185eb473818ef798d70
    Reviewed-by: Alex Blasche <alexander.blasche@theqtcompany.com>

--- examples/bluetooth/heartlistener/heartrate.cpp
+++ examples/bluetooth/heartlistener/heartrate.cpp
@@ -152,8 +152,15 @@ void HeartRate::connectToService(const QString &address)
 
     }
     //! [Connect signals]
+#ifdef Q_OS_MAC
+    // with CoreBluetooth controller requires QBluetoothDeviceInfo to connect:
+    m_control = new QLowEnergyController(m_currentDevice.getDevice(),
+                                            this);
+
+#else
     m_control = new QLowEnergyController(m_currentDevice.getDevice().address(),
                                             this);
+#endif
     connect(m_control, SIGNAL(serviceDiscovered(QBluetoothUuid)),
             this, SLOT(serviceDiscovered(QBluetoothUuid)));
     connect(m_control, SIGNAL(discoveryFinished()),
