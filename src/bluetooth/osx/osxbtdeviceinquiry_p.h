6b258fb24019811af6ca1f5449daeee02c58014d
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-10-08 15:47:38 +0200

    Add missing "We mean it" comments to private headers.
    Change-Id: I4e78a2b263fb779253556201fd20fe47a2afc58e
    Reviewed-by: Alex Blasche <alexander.blasche@theqtcompany.com>

--- src/bluetooth/osx/osxbtdeviceinquiry_p.h
+++ src/bluetooth/osx/osxbtdeviceinquiry_p.h
@@ -34,6 +34,17 @@
 #ifndef OSXBTDEVICEINQUIRY_P_H
 #define OSXBTDEVICEINQUIRY_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtCore/qglobal.h>
 
 // We have to import objc code (it does not have inclusion guards).
