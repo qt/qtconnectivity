ad431bbfc15efbe62912d4cdc26025b0ea51b324
Alex Blasche <alexander.blasche@theqtcompany.com>
2016-01-15 10:57:22 +0100

    Fix qdoc warning about missing dtor documentation in QQmlNdefRecord
    Change-Id: Ic1f3ca5075174addfbc6d76f31d9b89d2c654d19
    Reviewed-by: Christian Kandeler <christian.kandeler@theqtcompany.com>

--- src/nfc/qqmlndefrecord.cpp
+++ src/nfc/qqmlndefrecord.cpp
@@ -243,6 +243,9 @@ QQmlNdefRecord::QQmlNdefRecord(const QNdefRecord &record, QObject *parent)
     d_ptr->record = record;
 }
 
+/*!
+    Destroys the QQmlNdefRecord instance.
+*/
 QQmlNdefRecord::~QQmlNdefRecord()
 {
     delete d_ptr;
