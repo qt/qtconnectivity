09d30048e6cf8a86507bda1872fd7d9f653ec9ed
BogDan Vatra <bogdan@kdab.com>
2016-07-20 15:46:32 +0300

    Make sure JNI_OnLoad is not called more than once
    Since Android 5.0 Google introduce a nasty bug[1] which calls
    JNI_OnLoad more than once.
    Basically every time when a library is loaded JNI_OnLoad is
    called if found, but it calls *again* JNI_OnLoad of its .so
    dependencies!
    
    [1] Workaround https://code.google.com/p/android/issues/detail?id=215069
    
    Change-Id: I2894dd24afa9c51582f20ab44d83e9ce86751716
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>

--- src/nfc/android/androidjninfc.cpp
+++ src/nfc/android/androidjninfc.cpp
@@ -90,6 +90,11 @@ QT_END_ANDROIDNFC_NAMESPACE
 
 Q_DECL_EXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void * /*reserved*/)
 {
+    static bool initialized = false;
+    if (initialized)
+        return JNI_VERSION_1_6;
+    initialized = true;
+
     JNIEnv* env;
     if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
         return -1;
