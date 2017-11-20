a18338b552d45b6877e89221a5d6205081a54308
Lubomir I. Ivanov <lubomirivanov@vmware.com>
2017-11-20 16:27:59 +0100

    Add warning to private header qwinlowenergybluetooth_p.h
    Without this warning the build would not succeed.
    
    Change-Id: Ie43c494d80db2dfffa1f45c8896d996928678df0
    Reviewed-by: Denis Shienkov <denis.shienkov@gmail.com>
    Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

--- src/bluetooth/windows/qwinlowenergybluetooth_p.h
+++ src/bluetooth/windows/qwinlowenergybluetooth_p.h
@@ -35,6 +35,17 @@
 #ifndef QWINLOWENERGYBLUETOOTH_P_H
 #define QWINLOWENERGYBLUETOOTH_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtCore/qlibrary.h>
 
 #include <qt_windows.h>
