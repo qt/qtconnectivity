qt/qtconnectivity

commit 7d7651fb3bd0c2bdeb6159202d30cf475611da58

Don't build when there is no Qt Network
Qt Connectivity depends on QAbstractSocket, so it will not build if
Qt Network is not built.

Change-Id: I94f812f4b4c0c23a38b6c0fcdef0ee962f68e8b0
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

qtconnectivity.pro
@@ -1,3 +1,4 @@
-requires(!android|qtHaveModule(androidextras))
+requires(qtHaveModule(network):(!android|qtHaveModule(androidextras)))
+
 
 load(qt_parts)

commit 69e3e63ed9c31978d0663c87351c1b6918716d28

Fix qtconnectivity build
This patch fixes the error introduced by
7d7651fb3bd0c2bdeb6159202d30cf475611da58.

Change-Id: Idaf9066190535dd521efed7ab78e9203b99aadef
Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

qtconnectivity.pro
@@ -1,4 +1,5 @@
-requires(qtHaveModule(network):(!android|qtHaveModule(androidextras)))
+requires(qtHaveModule(network))
+requires(!android|qtHaveModule(androidextras))
 
 
 load(qt_parts)