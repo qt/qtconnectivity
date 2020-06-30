qt/qtconnectivity

commit 7a0db5d4984735587e1647d8b3ddb437d04945b3

Remove the registration mechanism from QNearFieldManager
The removal is proposed by a comment in qNearfieldmanager.h.

[ChangeLog][QtNFC][QNearFieldManager] Removed functions
(un)registerNdefMessageHandler(...).

Task-number: QTBUG-81824
Change-Id: Icf9dc4746a437a775ac40bbe68814daabd1ec13f
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

examples/nfc/annotatedurl/annotatedurl.cpp
@@ -67,27 +67,6 @@
 AnnotatedUrl::AnnotatedUrl(QObject *parent)
 :   QObject(parent)
 {
-    //! [QNearFieldManager register handler]
-    manager = new QNearFieldManager(this);
-    if (!manager->isEnabled()) {
-        qWarning() << "NFC not enabled";
-        return;
-    }
-
-    QNdefFilter filter;
-    filter.setOrderMatch(false);
-    filter.appendRecord<QNdefNfcTextRecord>(1, UINT_MAX);
-    filter.appendRecord<QNdefNfcUriRecord>();
-    // type parameter cannot specify substring so filter for "image/" below
-    filter.appendRecord(QNdefRecord::Mime, QByteArray(), 0, 1);
-
-    int result = manager->registerNdefMessageHandler(filter, this,
-                                       SLOT(handleMessage(QNdefMessage,QNearFieldTarget*)));
-    //! [QNearFieldManager register handler]
-
-    if (result < 0)
-        qWarning() << "Platform does not support NDEF message handler registration";
-
     manager->startTargetDetection();
     connect(manager, &QNearFieldManager::targetDetected,
             this, &AnnotatedUrl::targetDetected);

src/nfc/doc/snippets/doc_src_qtnfc.cpp
@@ -1,82 +1,0 @@
-/****************************************************************************
-**
-** Copyright (C) 2013 Aaron McCarthy <mccarthy.aaron@gmail.com>
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the documentation of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
-#include <QtCore/QObject>
-//! [include]
-#include <QtNfc/QNearFieldManager>
-//! [include]
-#include <QtNfc/QNdefMessage>
-
-//! [namespace]
-QT_USE_NAMESPACE
-//! [namespace]
-
-class MyClass : public QObject
-{
-    Q_OBJECT
-public:
-    MyClass() : QObject()
-    {
-//formatting adjusted to improve usage in docs
-//! [handleNdefMessage]
-QNearFieldManager *manager = new QNearFieldManager(this);
-manager->registerNdefMessageHandler(this,
-            SLOT(handleNdefMessage(QNdefMessage,QNearFieldTarget*)));
-//! [handleNdefMessage]
-    }
-
-public Q_SLOTS:
-    void handleNdefMessage(QNdefMessage,QNearFieldTarget*)
-    {
-    }
-};
-
-#include "doc_src_qtnfc.moc"
-

src/nfc/doc/snippets/snippets.pro
@@ -6,5 +6,4 @@ QT += nfc
 #! [project modification]
 
 SOURCES += main.cpp \
-           doc_src_qtnfc.cpp \
            nfc.cpp

src/nfc/doc/src/nfc-overview.qdoc
@@ -107,35 +107,6 @@ As the above code demonstrates, writing of NDEF messages is triggered via
 is indicated by the emission of the \l QNearFieldTarget::ndefMessagesWritten() signal. Any
 type of error during read or write is indicated via \l QNearFieldTarget::error().
 
-\section2 Registering NDEF Message Handlers
-
-The above described method (of reading NDEF messages) directly connects to the platform's NFC infrastructure.
-However on some platforms (in particular mobile platforms) this may not actually trigger the target slot
-if the application is currently running in the background. This is not desirable in cases where an
-application wants to be activated if the platform detects a tag of particular type.
-For this purpose the Qt NFC API provides the possibility to register an NDEF message handler. The handler
-is called by the operating system, when the detected NDEF message matches the given filter criteria.
-Depending on the platform it may even be possible to start the application that registered the handler.
-
-\note This feature is not available on all platforms and, in addition to the code snippets below,
-may require further platform specific setup.
-
-\snippet annotatedurl/annotatedurl.cpp QNearFieldManager register handler
-
-For comparison an application that uses an empty NDEF filter (match all behavior) in combination with
-\l QNearFieldManager::registerNdefMessageHandler() would behave similarly to another application that uses
-\l QNearFieldTarget::readNdefMessages() while being in the forground. For more information about
-registration details of NDEF message handlers, see the
-\l {QNearFieldManager#automatically-launching-ndef-message-handlers}{QNearFieldManager} class description.
-
-The content of \c handleMessage() may look like the snippet below. Any incoming NDEF message of type
-\c text or \c uri will be processed:
-
-\snippet annotatedurl/annotatedurl.cpp handleMessage 1
-\snippet annotatedurl/annotatedurl.cpp handleMessage 2
-\snippet annotatedurl/annotatedurl.cpp handleMessage 3
-\snippet annotatedurl/annotatedurl.cpp handleMessage 4
-
 \section2 Sharing Files and Messages
 
 Since Qt 5.3, Qt NFC provides a generic NFC share feature. If both devices support the same protocol,

src/nfc/qnearfieldmanager.cpp
@@ -72,11 +72,6 @@ QT_BEGIN_NAMESPACE
     are also capable of operating as the slave, so called Card Emulation mode. In this mode the
     local NFC device emulates a NFC Forum Tag or Contactless Card.
 
-    NFC Forum Tags can contain one or more messages in a standardized format. These messages are
-    encapsulated by the QNdefMessage class. Use the registerNdefMessageHandler() functions to
-    register message handlers with particular criteria. Handlers can be unregistered with the
-    unregisterNdefMessageHandler() function.
-
     Applications can connect to the targetDetected() and targetLost() signals to get notified when
     an NFC Forum Tag enters or leaves proximity. Before these signals are
     emitted target detection must be started with the startTargetDetection() function.
@@ -87,25 +82,6 @@ QT_BEGIN_NAMESPACE
     perform on the detected target. When access is no longer required the target access modes
     should be set to NoTargetAccess as other applications may be blocked from accessing targets.
     The current target access modes can be retried with the targetAccessModes() function.
-
-
-    \section2 Automatically launching NDEF message handlers
-
-    On some platforms it is possible to pre-register an application to receive NDEF messages
-    matching a given criteria. This is useful to get the system to automatically launch your
-    application when a matching NDEF message is received. This removes the need to have the user
-    manually launch NDEF handling applications, prior to touching a tag, or to have those
-    applications always running and using system resources.
-
-    The process of registering the handler is different for each platform. Please refer to the
-    platform documentation on how such a registration may be done.
-    If the application has been registered as an NDEF message handler, the application only needs
-    to call the registerNdefMessageHandler() function:
-
-    \snippet doc_src_qtnfc.cpp handleNdefMessage
-
-    Automatically launching NDEF message handlers is supported on
-    \l{nfc-android.html}{Android}.
 */
 
 /*!
@@ -272,133 +248,6 @@ void QNearFieldManager::stopTargetDetection()
     Q_D(QNearFieldManager);
 
     d->stopTargetDetection();
-}
-
-static QMetaMethod methodForSignature(QObject *object, const char *method)
-{
-    QByteArray normalizedMethod = QMetaObject::normalizedSignature(method);
-
-    if (!QMetaObject::checkConnectArgs(SIGNAL(targetDetected(QNdefMessage,QNearFieldTarget*)),
-                                       normalizedMethod)) {
-        qWarning("Signatures do not match: %s:%d\n", __FILE__, __LINE__);
-        return QMetaMethod();
-    }
-
-    quint8 memcode = (normalizedMethod.at(0) - '0') & 0x03;
-    normalizedMethod = normalizedMethod.mid(1);
-
-    int index;
-    switch (memcode) {
-    case QSLOT_CODE:
-        index = object->metaObject()->indexOfSlot(normalizedMethod.constData());
-        break;
-    case QSIGNAL_CODE:
-        index = object->metaObject()->indexOfSignal(normalizedMethod.constData());
-        break;
-    case QMETHOD_CODE:
-        index = object->metaObject()->indexOfMethod(normalizedMethod.constData());
-        break;
-    default:
-        index = -1;
-    }
-
-    if (index == -1)
-        return QMetaMethod();
-
-    return object->metaObject()->method(index);
-}
-
-/*!
-    Registers \a object to receive notifications on \a method when a tag has been detected and has
-    an NDEF record that matches \a typeNameFormat and \a type.  The \a method on \a object should
-    have the prototype
-    'void targetDetected(const QNdefMessage &message, QNearFieldTarget *target)'.
-
-    Returns an identifier, which can be used to unregister the handler, on success; otherwise
-    returns -1.
-
-    \note The \e target parameter of \a method may not be available on all platforms, in which case
-    \e target will be 0.
-*/
-
-int QNearFieldManager::registerNdefMessageHandler(QNdefRecord::TypeNameFormat typeNameFormat,
-                                                  const QByteArray &type,
-                                                  QObject *object, const char *method)
-{
-    QMetaMethod metaMethod = methodForSignature(object, method);
-    if (!metaMethod.enclosingMetaObject())
-        return -1;
-
-    QNdefFilter filter;
-    filter.appendRecord(typeNameFormat, type);
-
-    Q_D(QNearFieldManager);
-
-    return d->registerNdefMessageHandler(filter, object, metaMethod);
-}
-
-/*!
-    \fn int QNearFieldManager::registerNdefMessageHandler(QObject *object, const char *method)
-
-    Registers \a object to receive notifications on \a method when a tag has been detected and has
-    an NDEF message that matches a pre-registered message format. The \a method on \a object should
-    have the prototype
-    'void targetDetected(const QNdefMessage &message, QNearFieldTarget *target)'.
-
-    Returns an identifier, which can be used to unregister the handler, on success; otherwise
-    returns -1.
-
-    This function is used to register a QNearFieldManager instance to receive notifications when a
-    NDEF message matching a pre-registered message format is received. See the section on
-    \l {Automatically launching NDEF message handlers}.
-
-    \note The \e target parameter of \a method may not be available on all platforms, in which case
-    \e target will be 0.
-*/
-int QNearFieldManager::registerNdefMessageHandler(QObject *object, const char *method)
-{
-    QMetaMethod metaMethod = methodForSignature(object, method);
-    if (!metaMethod.enclosingMetaObject())
-        return -1;
-
-    Q_D(QNearFieldManager);
-
-    return d->registerNdefMessageHandler(object, metaMethod);
-}
-
-/*!
-    Registers \a object to receive notifications on \a method when a tag has been detected and has
-    an NDEF message that matches \a filter is detected. The \a method on \a object should have the
-    prototype 'void targetDetected(const QNdefMessage &message, QNearFieldTarget *target)'.
-
-    Returns an identifier, which can be used to unregister the handler, on success; otherwise
-    returns -1.
-
-    \note The \e target parameter of \a method may not be available on all platforms, in which case
-    \e target will be 0.
-*/
-int QNearFieldManager::registerNdefMessageHandler(const QNdefFilter &filter,
-                                                  QObject *object, const char *method)
-{
-    QMetaMethod metaMethod = methodForSignature(object, method);
-    if (!metaMethod.enclosingMetaObject())
-        return -1;
-
-    Q_D(QNearFieldManager);
-
-    return d->registerNdefMessageHandler(filter, object, metaMethod);
-}
-
-/*!
-    Unregisters the target detect handler identified by \a handlerId.
-
-    Returns true on success; otherwise returns false.
-*/
-bool QNearFieldManager::unregisterNdefMessageHandler(int handlerId)
-{
-    Q_D(QNearFieldManager);
-
-    return d->unregisterNdefMessageHandler(handlerId);
 }
 
 /*!

src/nfc/qnearfieldmanager.h
@@ -85,19 +85,6 @@ public:
     bool startTargetDetection();
     void stopTargetDetection();
 
-    //TODO Qt 6 Consider removal of this registration mechanism
-    //None of the currently supported platforms supports the feature
-    //or in fact the implementation (on Android) is not what the
-    //function is supposed to do.
-    int registerNdefMessageHandler(QObject *object, const char *method);
-    int registerNdefMessageHandler(QNdefRecord::TypeNameFormat typeNameFormat,
-                                   const QByteArray &type,
-                                   QObject *object, const char *method);
-    int registerNdefMessageHandler(const QNdefFilter &filter,
-                                   QObject *object, const char *method);
-
-    bool unregisterNdefMessageHandler(int handlerId);
-
 Q_SIGNALS:
     void adapterStateChanged(QNearFieldManager::AdapterState state);
     void targetDetected(QNearFieldTarget *target);

src/nfc/qnearfieldmanager_android.cpp
@@ -227,53 +227,6 @@ void QNearFieldManagerPrivateImpl::stopTargetDetection()
     updateReceiveState();
 }
 
-// FIXME This is supposed to be a platform registration. A message that
-// matches the given NDEF filter should restart the current application.
-// The implementation below only works as long as the current application
-// is running. It is not a platform wide registration on Android.
-int QNearFieldManagerPrivateImpl::registerNdefMessageHandler(QObject *object, const QMetaMethod &method)
-{
-    ndefMessageHandlers.append(QPair<QPair<int, QObject *>, QMetaMethod>(QPair<int, QObject *>(m_handlerID, object), method));
-    updateReceiveState();
-    //Returns the handler ID and increments it afterwards
-    return m_handlerID++;
-}
-
-// FIXME see above
-int QNearFieldManagerPrivateImpl::registerNdefMessageHandler(const QNdefFilter &filter,
-                                                             QObject *object, const QMetaMethod &method)
-{
-    //If no record is set in the filter, we ignore the filter
-    if (filter.recordCount()==0)
-        return registerNdefMessageHandler(object, method);
-
-    ndefFilterHandlers.append(QPair<QPair<int, QObject*>, QPair<QNdefFilter, QMetaMethod> >
-                              (QPair<int, QObject*>(m_handlerID, object), QPair<QNdefFilter, QMetaMethod>(filter, method)));
-
-    updateReceiveState();
-
-    return m_handlerID++;
-}
-
-bool QNearFieldManagerPrivateImpl::unregisterNdefMessageHandler(int handlerId)
-{
-    for (int i=0; i<ndefMessageHandlers.count(); ++i) {
-        if (ndefMessageHandlers.at(i).first.first == handlerId) {
-            ndefMessageHandlers.removeAt(i);
-            updateReceiveState();
-            return true;
-        }
-    }
-    for (int i=0; i<ndefFilterHandlers.count(); ++i) {
-        if (ndefFilterHandlers.at(i).first.first == handlerId) {
-            ndefFilterHandlers.removeAt(i);
-            updateReceiveState();
-            return true;
-        }
-    }
-    return false;
-}
-
 void QNearFieldManagerPrivateImpl::requestAccess(QNearFieldManager::TargetAccessModes accessModes)
 {
     Q_UNUSED(accessModes);

src/nfc/qnearfieldmanager_android_p.h
@@ -79,9 +79,6 @@ public:
     bool isSupported() const override;
     bool startTargetDetection() override;
     void stopTargetDetection() override;
-    int registerNdefMessageHandler(QObject *object, const QMetaMethod &method) override;
-    int registerNdefMessageHandler(const QNdefFilter &filter, QObject *object, const QMetaMethod &method) override;
-    bool unregisterNdefMessageHandler(int handlerId) override;
     void requestAccess(QNearFieldManager::TargetAccessModes accessModes) override;
     void releaseAccess(QNearFieldManager::TargetAccessModes accessModes) override;
     void newIntent(QAndroidJniObject intent) override;

commit 2bf5b2bf37d13a970be06a569067c503c3064db3

Fix crashing NFC annotated example
This was accidentally caused by 7a0db5d4984735587e1647d8b3ddb437d04945b3.

Change-Id: I2325539687319ec8e99a70c62593f8f6b62cca9d
Reviewed-by: Lars Schmertmann <lars.schmertmann@governikus.de>
Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

examples/nfc/annotatedurl/annotatedurl.cpp
@@ -67,6 +67,12 @@
 AnnotatedUrl::AnnotatedUrl(QObject *parent)
 :   QObject(parent)
 {
+    manager = new QNearFieldManager(this);
+    if (!manager->isEnabled()) {
+        qWarning() << "NFC not enabled";
+        return;
+    }
+
     manager->startTargetDetection();
     connect(manager, &QNearFieldManager::targetDetected,
             this, &AnnotatedUrl::targetDetected);