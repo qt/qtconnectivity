5616af2c86356228ebefba6205a82b55b44e378a
Andreas Buhr <andreas.buhr@qt.io>
2021-03-25 15:58:42 +0100

    Register enums in QBluetooth with the meta object system
    This patch registers the two enums
    QBluetooth::Security and
    QBluetooth::AttAccessConstraint
    with the meta-object system. Thus they can easily
    be printed in logging messages.
    
    Task-number: QTBUG-91906
    Change-Id: I0124215ea1578d674d4dc248da7c0fc75723f21d
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

--- src/bluetooth/qbluetooth.h
+++ src/bluetooth/qbluetooth.h
@@ -41,11 +41,14 @@
 #define QBLUETOOTH_H
 
 #include <QtBluetooth/qtbluetoothglobal.h>
+#include <QtCore/qglobal.h>
+#include <QtCore/qtmetamacros.h>
+#include <QtCore/qobject.h>
 
 QT_BEGIN_NAMESPACE
 
 namespace QBluetooth {
-
+Q_NAMESPACE_EXPORT(Q_BLUETOOTH_EXPORT)
 // TODO Qt 6: Merge these two enums? But note that ATT Authorization has no equivalent
 //            on the socket security level.
 
@@ -56,7 +59,7 @@ enum Security {
     Encryption = 0x04,
     Secure = 0x08
 };
-
+Q_ENUM_NS(Security)
 Q_DECLARE_FLAGS(SecurityFlags, Security)
 Q_DECLARE_OPERATORS_FOR_FLAGS(SecurityFlags)
 
@@ -65,6 +68,7 @@ enum AttAccessConstraint {
     AttAuthenticationRequired = 0x2,
     AttEncryptionRequired = 0x4,
 };
+Q_ENUM_NS(AttAccessConstraint)
 
 Q_DECLARE_FLAGS(AttAccessConstraints, AttAccessConstraint)
 Q_DECLARE_OPERATORS_FOR_FLAGS(AttAccessConstraints)
