f2d15c8f8104b05b6a53712b2ff7a5fcaef033d1
Oliver Wolff <oliver.wolff@qt.io>
2021-04-07 13:28:41 +0200

    Port QBluetoothLocalDevice_winrt to c++/winrt
    Change-Id: I05062d40a6ae8b09f0b267dff2203f36c885d23e
    Reviewed-by: Alex Blasche <alexander.blasche@qt.io>
    (cherry picked from commit d6dc79a13c9066b4c5cee217524da02f28dbf359)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/bluetooth/qbluetoothlocaldevice_p.h
+++ src/bluetooth/qbluetoothlocaldevice_p.h
@@ -80,21 +80,6 @@ QT_END_NAMESPACE
 #include <QtCore/QPair>
 #endif
 
-#ifdef QT_WINRT_BLUETOOTH
-#include <wrl.h>
-
-namespace ABI {
-    namespace Windows {
-        namespace Devices {
-            namespace Bluetooth {
-                struct IBluetoothDeviceStatics;
-                struct IBluetoothLEDeviceStatics;
-            }
-        }
-    }
-}
-#endif
-
 QT_BEGIN_NAMESPACE
 
 extern void registerQBluetoothLocalDeviceMetaType();
@@ -207,8 +192,6 @@ public:
 
 private:
     QBluetoothLocalDevice *q_ptr;
-    Microsoft::WRL::ComPtr<ABI::Windows::Devices::Bluetooth::IBluetoothDeviceStatics> mStatics;
-    Microsoft::WRL::ComPtr<ABI::Windows::Devices::Bluetooth::IBluetoothLEDeviceStatics> mLEStatics;
 };
 #elif !defined(QT_OSX_BLUETOOTH) // dummy backend
 class QBluetoothLocalDevicePrivate : public QObject
