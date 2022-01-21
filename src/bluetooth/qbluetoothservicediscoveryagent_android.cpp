qt/qtconnectivity

commit 66bb9839dbccea311cbb85168da86fb6d4a2d136

Use scoped enum for constants in QBluetoothUuid
QBluetoothUuid contains enums for ProtocolUuid, ServiceClassUuid,
CharacteristicType and DescriptorType. So far, they all
put their constants directly into the QBluetoothUuid
namespace, making it easy to mix them up. This patch changes
those to scoped enums. That way, each enum has its items in
its own namespace.

Change-Id: I86ea08ff31009dc8073d84cfe678e27920d693f7
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

src/bluetooth/qbluetoothservicediscoveryagent_android.cpp
@@ -365,8 +365,7 @@ void QBluetoothServiceDiscoveryAgentPrivate::populateDiscoveredServices(const QB
 
         //check for SPP protocol
         bool ok = false;
-        auto uuid16 = uuid.toUInt16(&ok);
-        haveSppClass |= ok && uuid16 == QBluetoothUuid::SerialPort;
+        haveSppClass |= ok && uuid == QBluetoothUuid::ServiceClassUuid::SerialPort;
 
         //check for custom uuid
         if (uuid.minimumSize() == 16)
@@ -375,7 +374,7 @@ void QBluetoothServiceDiscoveryAgentPrivate::populateDiscoveredServices(const QB
 
     auto rfcommProtocolDescriptorList = []() -> QBluetoothServiceInfo::Sequence {
             QBluetoothServiceInfo::Sequence protocol;
-            protocol << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::Rfcomm))
+            protocol << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::ProtocolUuid::Rfcomm))
                      << QVariant::fromValue(0);
             return protocol;
     };
@@ -383,7 +382,7 @@ void QBluetoothServiceDiscoveryAgentPrivate::populateDiscoveredServices(const QB
     auto sppProfileDescriptorList = []() -> QBluetoothServiceInfo::Sequence {
         QBluetoothServiceInfo::Sequence profileSequence;
         QBluetoothServiceInfo::Sequence classId;
-        classId << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::SerialPort));
+        classId << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::ServiceClassUuid::SerialPort));
         classId << QVariant::fromValue(quint16(0x100));
         profileSequence.append(QVariant::fromValue(classId));
         return profileSequence;
@@ -400,7 +399,7 @@ void QBluetoothServiceDiscoveryAgentPrivate::populateDiscoveredServices(const QB
         QBluetoothServiceInfo::Sequence protocolDescriptorList;
         {
             QBluetoothServiceInfo::Sequence protocol;
-            protocol << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::L2cap));
+            protocol << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::ProtocolUuid::L2cap));
             protocolDescriptorList.append(QVariant::fromValue(protocol));
         }
 
@@ -417,12 +416,12 @@ void QBluetoothServiceDiscoveryAgentPrivate::populateDiscoveredServices(const QB
             QBluetoothServiceInfo::Sequence classId;
             //set SPP service class uuid
             classId << QVariant::fromValue(uuid);
-            classId << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::SerialPort));
+            classId << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::ServiceClassUuid::SerialPort));
             serviceInfo.setAttribute(QBluetoothServiceInfo::ServiceClassIds, classId);
 
             serviceInfo.setServiceName(QBluetoothServiceDiscoveryAgent::tr("Serial Port Profile"));
             serviceInfo.setServiceUuid(uuid);
-        } else if (uuid == QBluetoothUuid{QBluetoothUuid::SerialPort}) {
+        } else if (uuid == QBluetoothUuid{QBluetoothUuid::ServiceClassUuid::SerialPort}) {
             //set rfcomm protocol
             protocolDescriptorList.append(QVariant::fromValue(rfcommProtocolDescriptorList()));
 
@@ -440,7 +439,7 @@ void QBluetoothServiceDiscoveryAgentPrivate::populateDiscoveredServices(const QB
 
         serviceInfo.setAttribute(QBluetoothServiceInfo::ProtocolDescriptorList, protocolDescriptorList);
         QBluetoothServiceInfo::Sequence publicBrowse;
-        publicBrowse << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::PublicBrowseGroup));
+        publicBrowse << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::ServiceClassUuid::PublicBrowseGroup));
         serviceInfo.setAttribute(QBluetoothServiceInfo::BrowseGroupList, publicBrowse);
 
         if (!customUuids.contains(i)) {

examples/bluetooth/btchat/chatserver.cpp
@@ -87,7 +87,7 @@ void ChatServer::startServer(const QBluetoothAddress& localAdapter)
 
     QBluetoothServiceInfo::Sequence profileSequence;
     QBluetoothServiceInfo::Sequence classId;
-    classId << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::SerialPort));
+    classId << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::ServiceClassUuid::SerialPort));
     classId << QVariant::fromValue(quint16(0x100));
     profileSequence.append(QVariant::fromValue(classId));
     serviceInfo.setAttribute(QBluetoothServiceInfo::BluetoothProfileDescriptorList,
@@ -95,7 +95,7 @@ void ChatServer::startServer(const QBluetoothAddress& localAdapter)
 
     classId.clear();
     classId << QVariant::fromValue(QBluetoothUuid(serviceUuid));
-    classId << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::SerialPort));
+    classId << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::ServiceClassUuid::SerialPort));
 
     serviceInfo.setAttribute(QBluetoothServiceInfo::ServiceClassIds, classId);
 
@@ -112,7 +112,7 @@ void ChatServer::startServer(const QBluetoothAddress& localAdapter)
 
     //! [Service Discoverability]
     QBluetoothServiceInfo::Sequence publicBrowse;
-    publicBrowse << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::PublicBrowseGroup));
+    publicBrowse << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::ServiceClassUuid::PublicBrowseGroup));
     serviceInfo.setAttribute(QBluetoothServiceInfo::BrowseGroupList,
                              publicBrowse);
     //! [Service Discoverability]
@@ -120,10 +120,10 @@ void ChatServer::startServer(const QBluetoothAddress& localAdapter)
     //! [Protocol descriptor list]
     QBluetoothServiceInfo::Sequence protocolDescriptorList;
     QBluetoothServiceInfo::Sequence protocol;
-    protocol << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::L2cap));
+    protocol << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::ProtocolUuid::L2cap));
     protocolDescriptorList.append(QVariant::fromValue(protocol));
     protocol.clear();
-    protocol << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::Rfcomm))
+    protocol << QVariant::fromValue(QBluetoothUuid(QBluetoothUuid::ProtocolUuid::Rfcomm))
              << QVariant::fromValue(quint8(rfcommServer->serverPort()));
     protocolDescriptorList.append(QVariant::fromValue(protocol));
     serviceInfo.setAttribute(QBluetoothServiceInfo::ProtocolDescriptorList,

examples/bluetooth/heartrate-game/devicehandler.cpp
@@ -164,7 +164,7 @@ void DeviceHandler::stopMeasurement()
 //! [Filter HeartRate service 1]
 void DeviceHandler::serviceDiscovered(const QBluetoothUuid &gatt)
 {
-    if (gatt == QBluetoothUuid(QBluetoothUuid::HeartRate)) {
+    if (gatt == QBluetoothUuid(QBluetoothUuid::ServiceClassUuid::HeartRate)) {
         setInfo("Heart Rate service discovered. Waiting for service scan to be done...");
         m_foundHeartRateService = true;
     }
@@ -184,7 +184,7 @@ void DeviceHandler::serviceScanDone()
 //! [Filter HeartRate service 2]
     // If heartRateService found, create new service
     if (m_foundHeartRateService)
-        m_service = m_control->createServiceObject(QBluetoothUuid(QBluetoothUuid::HeartRate), this);
+        m_service = m_control->createServiceObject(QBluetoothUuid(QBluetoothUuid::ServiceClassUuid::HeartRate), this);
 
     if (m_service) {
         connect(m_service, &QLowEnergyService::stateChanged, this, &DeviceHandler::serviceStateChanged);
@@ -209,13 +209,13 @@ void DeviceHandler::serviceStateChanged(QLowEnergyService::ServiceState s)
     {
         setInfo(tr("Service discovered."));
 
-        const QLowEnergyCharacteristic hrChar = m_service->characteristic(QBluetoothUuid(QBluetoothUuid::HeartRateMeasurement));
+        const QLowEnergyCharacteristic hrChar = m_service->characteristic(QBluetoothUuid(QBluetoothUuid::CharacteristicType::HeartRateMeasurement));
         if (!hrChar.isValid()) {
             setError("HR Data not found.");
             break;
         }
 
-        m_notificationDesc = hrChar.descriptor(QBluetoothUuid::ClientCharacteristicConfiguration);
+        m_notificationDesc = hrChar.descriptor(QBluetoothUuid::DescriptorType::ClientCharacteristicConfiguration);
         if (m_notificationDesc.isValid())
             m_service->writeDescriptor(m_notificationDesc, QByteArray::fromHex("0100"));
 
@@ -234,7 +234,7 @@ void DeviceHandler::serviceStateChanged(QLowEnergyService::ServiceState s)
 void DeviceHandler::updateHeartRateValue(const QLowEnergyCharacteristic &c, const QByteArray &value)
 {
     // ignore any other characteristic change -> shouldn't really happen though
-    if (c.uuid() != QBluetoothUuid(QBluetoothUuid::HeartRateMeasurement))
+    if (c.uuid() != QBluetoothUuid(QBluetoothUuid::CharacteristicType::HeartRateMeasurement))
         return;
 
     auto data = reinterpret_cast<const quint8 *>(value.constData());

examples/bluetooth/heartrate-server/main.cpp
@@ -81,21 +81,21 @@ int main(int argc, char *argv[])
     advertisingData.setDiscoverability(QLowEnergyAdvertisingData::DiscoverabilityGeneral);
     advertisingData.setIncludePowerLevel(true);
     advertisingData.setLocalName("HeartRateServer");
-    advertisingData.setServices(QList<QBluetoothUuid>() << QBluetoothUuid::HeartRate);
+    advertisingData.setServices(QList<QBluetoothUuid>() << QBluetoothUuid::ServiceClassUuid::HeartRate);
     //! [Advertising Data]
 
     //! [Service Data]
     QLowEnergyCharacteristicData charData;
-    charData.setUuid(QBluetoothUuid::HeartRateMeasurement);
+    charData.setUuid(QBluetoothUuid::CharacteristicType::HeartRateMeasurement);
     charData.setValue(QByteArray(2, 0));
     charData.setProperties(QLowEnergyCharacteristic::Notify);
-    const QLowEnergyDescriptorData clientConfig(QBluetoothUuid::ClientCharacteristicConfiguration,
+    const QLowEnergyDescriptorData clientConfig(QBluetoothUuid::DescriptorType::ClientCharacteristicConfiguration,
                                                 QByteArray(2, 0));
     charData.addDescriptor(clientConfig);
 
     QLowEnergyServiceData serviceData;
     serviceData.setType(QLowEnergyServiceData::ServiceTypePrimary);
-    serviceData.setUuid(QBluetoothUuid::HeartRate);
+    serviceData.setUuid(QBluetoothUuid::ServiceClassUuid::HeartRate);
     serviceData.addCharacteristic(charData);
     //! [Service Data]
 
@@ -115,7 +115,7 @@ int main(int argc, char *argv[])
         value.append(char(0)); // Flags that specify the format of the value.
         value.append(char(currentHeartRate)); // Actual value.
         QLowEnergyCharacteristic characteristic
-                = service->characteristic(QBluetoothUuid::HeartRateMeasurement);
+                = service->characteristic(QBluetoothUuid::CharacteristicType::HeartRateMeasurement);
         Q_ASSERT(characteristic.isValid());
         service->writeCharacteristic(characteristic, value); // Potentially causes notification.
         if (currentHeartRate == 60)

examples/bluetooth/lowenergyscanner/characteristicinfo.cpp
@@ -74,7 +74,7 @@ QString CharacteristicInfo::getName() const
     // find descriptor with CharacteristicUserDescription
     const QList<QLowEnergyDescriptor> descriptors = m_characteristic.descriptors();
     for (const QLowEnergyDescriptor &descriptor : descriptors) {
-        if (descriptor.type() == QBluetoothUuid::CharacteristicUserDescription) {
+        if (descriptor.type() == QBluetoothUuid::DescriptorType::CharacteristicUserDescription) {
             name = descriptor.value();
             break;
         }

examples/bluetooth/lowenergyscanner/doc/src/lowenergyscanner.qdoc
@@ -132,7 +132,7 @@
     Although the example application does not display descriptors it uses descriptors to
     get the name of an individual characteristic if its name cannot be discerned based on its
     UUID. The second way to obtain the name is the existence of a descriptor of the type
-    \l {QBluetoothUuid::CharacteristicUserDescription}. The code below demonstrates how this
+    \l {QBluetoothUuid::DescriptorType::CharacteristicUserDescription}. The code below demonstrates how this
     may be achieved:
 
     \snippet lowenergyscanner/characteristicinfo.cpp les-get-descriptors

src/bluetooth/darwin/btcentralmanager.mm
@@ -683,7 +683,7 @@ QT_USE_NAMESPACE
                     newDesc.value = qt_bytearray(static_cast<NSObject *>(d.value));
                     descList[lastValidHandle] = newDesc;
                     // Check, if it's client characteristic configuration descriptor:
-                    if (newDesc.uuid == QBluetoothUuid::ClientCharacteristicConfiguration) {
+                    if (newDesc.uuid == QBluetoothUuid::DescriptorType::ClientCharacteristicConfiguration) {
                         if (newDesc.value.size() && (newDesc.value[0] & 3))
                             [peripheral setNotifyValue:YES forCharacteristic:c];
                     }
@@ -807,7 +807,7 @@ QT_USE_NAMESPACE
         CBCharacteristic *const characteristic = charMap[request.handle];
 
         if (request.type == LERequest::ClientConfiguration) {
-            const QBluetoothUuid qtUuid(QBluetoothUuid::ClientCharacteristicConfiguration);
+            const QBluetoothUuid qtUuid(QBluetoothUuid::DescriptorType::ClientCharacteristicConfiguration);
             CBDescriptor *const descriptor = [self descriptor:qtUuid forCharacteristic:characteristic];
             Q_ASSERT_X(descriptor, Q_FUNC_INFO, "no client characteristic "
                        "configuration descriptor found");
@@ -868,7 +868,7 @@ QT_USE_NAMESPACE
     // At the moment we call setNotifyValue _only_ from 'writeDescriptor';
     // from Qt's API POV it's a descriptor write operation and we must report
     // it back, so check _now_ that we really have this descriptor.
-    const QBluetoothUuid qtUuid(QBluetoothUuid::ClientCharacteristicConfiguration);
+    const QBluetoothUuid qtUuid(QBluetoothUuid::DescriptorType::ClientCharacteristicConfiguration);
     if (![self descriptor:qtUuid forCharacteristic:charMap[charHandle]]) {
         qCWarning(QT_BT_DARWIN) << "no client characteristic configuration found";
         if (notifier) {
@@ -1809,7 +1809,7 @@ QT_USE_NAMESPACE
 
     requestPending = false;
 
-    const QBluetoothUuid qtUuid(QBluetoothUuid::ClientCharacteristicConfiguration);
+    const QBluetoothUuid qtUuid(QBluetoothUuid::DescriptorType::ClientCharacteristicConfiguration);
     CBDescriptor *const descriptor = [self descriptor:qtUuid forCharacteristic:characteristic];
     const QByteArray valueToReport(valuesToWrite.value(descriptor, QByteArray()));
     const int nRemoved = valuesToWrite.remove(descriptor);

src/bluetooth/darwin/btperipheralmanager.mm
@@ -105,8 +105,8 @@ ObjCStrongReference<CBMutableDescriptor> create_descriptor(const QLowEnergyDescr
     CBUUIDCharacteristicUserDescriptionString and CBUUIDCharacteristicFormatString"
     */
 
-    if (data.uuid() != QBluetoothUuid::CharacteristicUserDescription &&
-        data.uuid() != QBluetoothUuid::CharacteristicPresentationFormat) {
+    if (data.uuid() != QBluetoothUuid::DescriptorType::CharacteristicUserDescription &&
+        data.uuid() != QBluetoothUuid::DescriptorType::CharacteristicPresentationFormat) {
         qCWarning(QT_BT_DARWIN) << "unsupported descriptor" << data.uuid();
         return {};
     }
@@ -116,7 +116,7 @@ ObjCStrongReference<CBMutableDescriptor> create_descriptor(const QLowEnergyDescr
     // Descriptors are immutable with CoreBluetooth, that's why we
     // have to provide a value here and not able to change it later.
     ObjCStrongReference<NSObject> value;
-    if (data.uuid() == QBluetoothUuid::CharacteristicUserDescription) {
+    if (data.uuid() == QBluetoothUuid::DescriptorType::CharacteristicUserDescription) {
         const QString asQString(QString::fromUtf8(data.value()));
         value.reset(asQString.toNSString(), RetainPolicy::doInitialRetain); // toNSString is auto-released, we have to retain.
     } else {

commit c7d2cf1127012e8be8d9cb4aed5ea7bd6718133c

Android: Fix SPP protocol detection during service discovery
A bug was introduced during refactoring in
66bb9839dbccea311cbb85168da86fb6d4a2d136.
After the refactoring the boolean 'ok' variable was always false, so
the condition in the right part was always evaluated to false.

Pick-to: 6.3 6.2
Change-Id: I25e98f7315778fa9f3fa22f2874d5641337c5715
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>
Reviewed-by: Juha Vuolle <juha.vuolle@insta.fi>

src/bluetooth/qbluetoothservicediscoveryagent_android.cpp
@@ -361,8 +361,7 @@ void QBluetoothServiceDiscoveryAgentPrivate::populateDiscoveredServices(const QB
             continue;
 
         //check for SPP protocol
-        bool ok = false;
-        haveSppClass |= ok && uuid == QBluetoothUuid::ServiceClassUuid::SerialPort;
+        haveSppClass |= uuid == QBluetoothUuid::ServiceClassUuid::SerialPort;
 
         //check for custom uuid
         if (uuid.minimumSize() == 16)