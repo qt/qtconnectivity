734d1114318fdcf487d67cd362e0eef041b0e136
Juha Vuolle <juha.vuolle@insta.fi>
2022-05-31 11:18:46 +0300

    Port QtConnectivity configure check reporting from Qt 5
    In addition reintroduce the 'bluez' compilation test which was
    omitted in the original Qt 6 port.
    
    Fixes: QTBUG-103209
    Change-Id: If82e85615f0b85f055d42f3de7662f05f2cbcd08
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 0b90663e08cdb3f61849dcfe94dd14a32546b648)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/bluetooth/configure.cmake
+++ src/bluetooth/configure.cmake
@@ -11,6 +11,11 @@ qt_find_package(BlueZ PROVIDED_TARGETS PkgConfig::BlueZ)
 
 #### Tests
 
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/../../config.tests/bluez/CMakeLists.txt")
+    qt_config_compile_test("bluez"
+                           LABEL "BlueZ"
+                           PROJECT_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../../config.tests/bluez")
+endif()
 
 if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/../../config.tests/bluez_le/CMakeLists.txt")
     qt_config_compile_test("bluez_le"
@@ -35,7 +40,7 @@ endif()
 
 qt_feature("bluez" PUBLIC
     LABEL "BlueZ"
-    CONDITION BLUEZ_FOUND AND QT_FEATURE_dbus # special case
+    CONDITION BLUEZ_FOUND AND TEST_bluez AND QT_FEATURE_dbus
 )
 qt_feature("bluez_le" PRIVATE
     LABEL "BlueZ Low Energy"
@@ -46,6 +51,22 @@ qt_feature("linux_crypto_api" PRIVATE
     CONDITION QT_FEATURE_bluez_le AND TEST_linux_crypto_api
 )
 qt_feature("winrt_bt" PRIVATE
-    LABEL "WinRT Bluetooth API (desktop & UWP)"
+    LABEL "WinRT Bluetooth API"
     CONDITION WIN32 AND TEST_winrt_bt
+)
+
+qt_configure_add_summary_section(NAME "Qt Bluetooth")
+qt_configure_add_summary_entry(ARGS bluez)
+qt_configure_add_summary_entry(ARGS bluez_le)
+qt_configure_add_summary_entry(ARGS linux_crypto_api)
+qt_configure_add_summary_entry(ARGS winrt_bt)
+qt_configure_add_report_entry(
+    TYPE NOTE
+    MESSAGE "Bluez version is too old to support Bluetooth Low Energy. Only classic Bluetooth will be available."
+    CONDITION QT_FEATURE_bluez AND NOT QT_FEATURE_bluez_le
+)
+qt_configure_add_report_entry(
+    TYPE NOTE
+    MESSAGE "Linux crypto API not present. BTLE signed writes will not work."
+    CONDITION QT_FEATURE_bluez_le AND NOT QT_FEATURE_linux_crypto_api
 )
