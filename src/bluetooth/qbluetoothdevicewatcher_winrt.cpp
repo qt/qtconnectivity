f7453b9ac69fd177ead408c22eb580a21797bef3
Ivan Solovev <ivan.solovev@qt.io>
2022-06-22 18:57:41 +0200

    Windows Bluetooth: move DeviceWatcher wrapper into a separate header
    The DeviceWatcher wrapper seems to be useful not only in
    QBluetoothLocalDevice implementation, but also for device discovery.
    This patch moves it to a separate header, so that it can be reused.
    The class' API is also refactored to suit for more general usecases.
    
    This patch also applies the changes to QBluetoothLocalDevice.
    
    As a drive-by: clean-up some includes and namespaces in
    QBluetoothLocalDevice implementation.
    
    Task-number: QTBUG-103263
    Change-Id: I470c6eab4810065c03d5905032f4288fa9d6de8e
    Reviewed-by: Juha Vuolle <juha.vuolle@insta.fi>
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>
    (cherry picked from commit 761a059d5a5ef97e97039f3a34b3a7f92944f1f0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ src/bluetooth/qbluetoothdevicewatcher_winrt.cpp
@@ -0,0 +1,109 @@
+// Copyright (C) 2022 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "qbluetoothdevicewatcher_winrt_p.h"
+
+#include <winrt/Windows.Foundation.Collections.h>
+
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::Devices::Enumeration;
+
+QT_BEGIN_NAMESPACE
+
+QBluetoothDeviceWatcherWinRT::QBluetoothDeviceWatcherWinRT(int id, winrt::hstring selector)
+    : m_id(id),
+      m_watcher(DeviceInformation::CreateWatcher(selector))
+{
+    qRegisterMetaType<winrt::hstring>("winrt::hstring");
+}
+
+QBluetoothDeviceWatcherWinRT::QBluetoothDeviceWatcherWinRT(int id, winrt::hstring selector,
+                                winrt::Windows::Devices::Enumeration::DeviceInformationKind kind)
+    : m_id(id)
+{
+    qRegisterMetaType<winrt::hstring>("winrt::hstring");
+    const winrt::param::iterable<winrt::hstring> extra {};
+    m_watcher = DeviceInformation::CreateWatcher(selector, extra, kind);
+}
+
+QBluetoothDeviceWatcherWinRT::~QBluetoothDeviceWatcherWinRT()
+{
+    if (m_watcher && m_initialized) {
+        stop();
+        unsubscribeFromEvents();
+        m_initialized = false;
+    }
+}
+
+bool QBluetoothDeviceWatcherWinRT::init()
+{
+    if (!m_watcher) {
+        qWarning("Windows failed to create an instance of DeviceWatcher. "
+                 "Detection of Bluetooth devices might not work correctly.");
+        return false;
+    }
+    if (!m_initialized) {
+        subscribeToEvents();
+        m_initialized = true;
+    }
+    return true;
+}
+
+void QBluetoothDeviceWatcherWinRT::start()
+{
+    if (m_watcher)
+        m_watcher.Start();
+}
+
+void QBluetoothDeviceWatcherWinRT::stop()
+{
+    if (m_watcher && canStop())
+        m_watcher.Stop();
+}
+
+void QBluetoothDeviceWatcherWinRT::subscribeToEvents()
+{
+    Q_ASSERT(m_watcher.Status() == DeviceWatcherStatus::Created);
+    // The callbacks are triggered from separate threads. So we capture
+    // thisPtr to make sure that the object is valid.
+    auto thisPtr = shared_from_this();
+    m_addedToken = m_watcher.Added([thisPtr](DeviceWatcher, const DeviceInformation &info) {
+        emit thisPtr->deviceAdded(info.Id(), thisPtr->m_id);
+    });
+    m_removedToken =
+            m_watcher.Removed([thisPtr](DeviceWatcher, const DeviceInformationUpdate &upd) {
+                emit thisPtr->deviceRemoved(upd.Id(), thisPtr->m_id);
+            });
+    m_updatedToken =
+            m_watcher.Updated([thisPtr](DeviceWatcher, const DeviceInformationUpdate &upd) {
+                emit thisPtr->deviceUpdated(upd.Id(), thisPtr->m_id);
+            });
+    // because of ambiguous declaration
+    using WinRtInspectable = winrt::Windows::Foundation::IInspectable;
+    m_enumerationToken =
+            m_watcher.EnumerationCompleted([thisPtr](DeviceWatcher, const WinRtInspectable &) {
+                emit thisPtr->enumerationCompleted(thisPtr->m_id);
+            });
+    m_stoppedToken = m_watcher.Stopped([thisPtr](DeviceWatcher, const WinRtInspectable &) {
+        emit thisPtr->watcherStopped(thisPtr->m_id);
+    });
+}
+
+void QBluetoothDeviceWatcherWinRT::unsubscribeFromEvents()
+{
+    m_watcher.Added(m_addedToken);
+    m_watcher.Removed(m_removedToken);
+    m_watcher.Updated(m_updatedToken);
+    m_watcher.EnumerationCompleted(m_enumerationToken);
+    m_watcher.Stopped(m_stoppedToken);
+}
+
+bool QBluetoothDeviceWatcherWinRT::canStop() const
+{
+    const auto status = m_watcher.Status();
+    // Also 'Aborted', but calling Stop() there is a no-op
+    return status == DeviceWatcherStatus::Started
+            || status == DeviceWatcherStatus::EnumerationCompleted;
+}
+
+QT_END_NAMESPACE
