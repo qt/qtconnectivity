2f560d044fec92e94e8438791aa5e4d9daced197
Ivan Solovev <ivan.solovev@qt.io>
2022-07-07 13:49:54 +0200

    Windows: fix Classic device discovery by porting to C++/WinRT
    Start porting QWinRTBluetoothDeviceDiscoveryWorker to C++/WinRT.
    Use DeviceWatcher to discover both paired and unpaired Bluetooth
    Classic devices.
    
    Unlike old COM implementation, in C++/WinRT each callback for
    IAsyncOperation comes in its own thread, so we need to care
    more about thread-safety. As a result, we had to use
    std::enable_shared_from_this on the
    QWinRTBluetoothDeviceDiscoveryWorker to guarantee that it is
    alive when the callback is triggered, and also to use
    QMetaObject::invokeMethod() to check for the scan finish
    criteria. This caused some further refactoring of
    QWinRTBluetoothDeviceDiscoveryWorker.
    
    This patch leaves some TODOs to handle LE device discovery. This
    will be implemented in the following patches.
    
    Task-number: QTBUG-103263
    Pick-to: 6.4 6.3 6.2
    Change-Id: I9128fe7a65f1a5aedcba427d6944372ecfe33f2f
    Reviewed-by: Juha Vuolle <juha.vuolle@insta.fi>
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

--- src/bluetooth/qbluetoothdevicediscoveryagent_p.h
+++ src/bluetooth/qbluetoothdevicediscoveryagent_p.h
@@ -150,7 +150,7 @@ private slots:
 
 private:
     void disconnectAndClearWorker();
-    QPointer<QWinRTBluetoothDeviceDiscoveryWorker> worker;
+    std::shared_ptr<QWinRTBluetoothDeviceDiscoveryWorker> worker = nullptr;
     QTimer *leScanTimer = nullptr;
 #endif
 
