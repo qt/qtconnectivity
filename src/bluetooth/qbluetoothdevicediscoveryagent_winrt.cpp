b7a7e31f95c09c3ac11260c18964665aae183c6b
Ivan Solovev <ivan.solovev@qt.io>
2022-08-11 12:04:35 +0200

    QWinRTBluetoothDeviceDiscoveryWorker: properly stop LE scan timer
    The LE scan timer was never stopped if the device discovery was finished
    earlier than the timer expires. That could lead to an application crash.
    This patch fixes it by making sure that the timer is stopped when
    QWinRTBluetoothDeviceDiscoveryWorker::stop() is called.
    
    This commit amends 9ab5122091a65936458c726270e693eca7a99277
    
    Task-number: QTBUG-103263
    Task-number: QTBUG-97797
    Change-Id: If10d9c1ef9f822b991225736ae36aceabe7e60a7
    Reviewed-by: Juha Vuolle <juha.vuolle@insta.fi>
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>
    (cherry picked from commit 0beadce4cf588633843f41fad51556559f9a6af7)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/bluetooth/qbluetoothdevicediscoveryagent_winrt.cpp
+++ src/bluetooth/qbluetoothdevicediscoveryagent_winrt.cpp
@@ -387,6 +387,8 @@ void QWinRTBluetoothDeviceDiscoveryWorker::start()
 
 void QWinRTBluetoothDeviceDiscoveryWorker::stop()
 {
+    if (m_leScanTimer && m_leScanTimer->isActive())
+        m_leScanTimer->stop();
     m_classicWatcher->stop();
     m_lowEnergyWatcher->stop();
     m_advertisementWatcher->stop();
