e9ef917dbaf2f0e85a68ea2392acf0a30674f2d2
Mårten Nordheim <marten.nordheim@qt.io>
2022-12-05 14:14:53 +0100

    PCSCLite: fix capitalization of Find-module file
    This fixes an issue where, upon reconfiguring, the package
    would not be looked up again (a side-effect of an optimization
    where we avoid looking up packages that have not been found).
    This lead to a "force feature ON breaks condition" error.
    
    Change-Id: I0be1bd6dd83186fe462d2dda9dbd93dfa372629a
    Reviewed-by: Ievgenii Meshcheriakov <ievgenii.meshcheriakov@qt.io>

--- src/nfc/configure.cmake
+++ src/nfc/configure.cmake
@@ -1,7 +1,7 @@
 # Copyright (C) 2022 The Qt Company Ltd.
 # SPDX-License-Identifier: BSD-3-Clause
 
-qt_find_package(PCSCLite PROVIDED_TARGETS PkgConfig::PCSCLite)
+qt_find_package(PCSCLITE PROVIDED_TARGETS PkgConfig::PCSCLITE)
 
 qt_feature("pcsclite" PUBLIC
     LABEL "PCSCLite"
