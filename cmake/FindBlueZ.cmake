c9a0ace545c9448f293f98686de3e30c1281ed29
Alexey Edelev <alexey.edelev@qt.io>
2022-12-23 10:38:24 +0100

    Fix the naming of the BlueZ package
    Adjust variable names to be case-sensitive.
    
    Fixes: QTBUG-109315
    Change-Id: I7d534fbfd7891740fbc8ac172b24a2b03832388b
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit 0b941a358063c58fe1eaaf3ed9884d70ce3992cb)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- cmake/FindBlueZ.cmake
+++ cmake/FindBlueZ.cmake
@@ -1,10 +1,12 @@
 # Copyright (C) 2022 The Qt Company Ltd.
 # SPDX-License-Identifier: BSD-3-Clause
 
+set(BlueZ_FOUND 0)
+
 find_package(PkgConfig QUIET)
 
 pkg_check_modules(BLUEZ bluez IMPORTED_TARGET)
 
-if (NOT TARGET PkgConfig::BLUEZ)
-    set(BLUEZ_FOUND 0)
+if(TARGET PkgConfig::BLUEZ)
+    set(BlueZ_FOUND 1)
 endif()
