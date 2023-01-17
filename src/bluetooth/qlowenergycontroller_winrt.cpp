qt/qtconnectivity

commit 0b60ca266f0fe27053a58eff3dbd903e3a1678ca

Close the GattDeviceService once we are done with it
The Windows implementation is missing a significant detail.
According to the docs, we need to call a Close() method when we are
done working with the GattDeviceService, so that its resources are
freed.

In practice that is not so easily achievable, because the lifetime
of the GattDeviceService instance can't be easily controlled due
to various async operations. Apart from that, we subscribe for
value changes of the characteristics that can notify.

As a result, the most reasonable solution is to implement a cache
of opened GattDeviceService instances. Objects from this cache are
reused each time we need to perform some operations on the service.
The cache is cleared when we disconnect from the device. Apart from
that we need to remove the specific instances from the cache when
we rediscover services and their details.

Task-number: QTBUG-94001
Pick-to: 6.3 6.2
Change-Id: I9f7cc55d7972591715e33081db6e43a97d5f0b67
Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>
Reviewed-by: Juha Vuolle <juha.vuolle@insta.fi>

src/bluetooth/qlowenergycontroller_winrt.cpp
@@ -138,6 +138,15 @@ static QByteArray byteArrayFromGattResult(const ComPtr<IGattReadResult> &gattRes
     return byteArrayFromBuffer(buffer, isWCharString);
 }
 
+static void closeDeviceService(ComPtr<IGattDeviceService> service)
+{
+    ComPtr<IClosable> closableService;
+    HRESULT hr = service.As(&closableService);
+    RETURN_IF_FAILED("Could not cast service to closable", return);
+    hr = closableService->Close();
+    RETURN_IF_FAILED("Service Close() failed", return);
+}
+
 class QWinRTLowEnergyServiceHandler : public QObject
 {
     Q_OBJECT
@@ -735,6 +744,7 @@ void QLowEnergyControllerPrivateWinRT::disconnectFromDevice()
     unregisterFromValueChanges();
     unregisterFromStatusChanges();
     unregisterFromMtuChanges();
+    clearAllServices();
     mGattSession = nullptr;
     mDevice = nullptr;
     setState(QLowEnergyController::UnconnectedState);
@@ -744,11 +754,16 @@ void QLowEnergyControllerPrivateWinRT::disconnectFromDevice()
 ComPtr<IGattDeviceService> QLowEnergyControllerPrivateWinRT::getNativeService(
         const QBluetoothUuid &serviceUuid)
 {
+    if (m_openedServices.contains(serviceUuid))
+        return m_openedServices.value(serviceUuid);
+
     ComPtr<IGattDeviceService> deviceService;
     HRESULT hr;
     hr = mDevice->GetGattService(serviceUuid, &deviceService);
     if (FAILED(hr))
         qCDebug(QT_BT_WINDOWS) << "Could not obtain native service for Uuid" << serviceUuid;
+    if (deviceService)
+        m_openedServices[serviceUuid] = deviceService;
     return deviceService;
 }
 
@@ -761,7 +776,7 @@ ComPtr<IGattCharacteristic> QLowEnergyControllerPrivateWinRT::getNativeCharacter
 
     ComPtr<IGattDeviceService3> service3;
     HRESULT hr = service.As(&service3);
-    RETURN_IF_FAILED("Could not cast service", return nullptr);
+    RETURN_IF_FAILED("Could not cast service to service3", return nullptr);
 
     ComPtr<IAsyncOperation<GattCharacteristicsResult *>> op;
     ComPtr<IGattCharacteristicsResult> result;
@@ -1079,10 +1094,26 @@ HRESULT QLowEnergyControllerPrivateWinRT::onServiceDiscoveryFinished(ABI::Window
     return S_OK;
 }
 
+void QLowEnergyControllerPrivateWinRT::clearAllServices()
+{
+    for (auto service : m_openedServices) {
+        closeDeviceService(service);
+    }
+    m_openedServices.clear();
+}
+
+void QLowEnergyControllerPrivateWinRT::closeAndRemoveService(const QBluetoothUuid &uuid)
+{
+    auto service = m_openedServices.take(uuid);
+    if (service)
+        closeDeviceService(service);
+}
+
 void QLowEnergyControllerPrivateWinRT::discoverServices()
 {
     qCDebug(QT_BT_WINDOWS) << "Service discovery initiated";
-
+    // clear the previous services cache, as we request the services again
+    clearAllServices();
     ComPtr<IBluetoothLEDevice3> device3;
     HRESULT hr = mDevice.As(&device3);
     CHECK_FOR_DEVICE_CONNECTION_ERROR(hr, "Could not cast device", return);
@@ -1106,6 +1137,9 @@ void QLowEnergyControllerPrivateWinRT::discoverServiceDetails(
         return;
     }
 
+    // clear the cache to rediscover service details
+    closeAndRemoveService(service);
+
     ComPtr<IGattDeviceService> deviceService = getNativeService(service);
     if (!deviceService) {
         qCDebug(QT_BT_WINDOWS) << "Could not obtain native service for uuid " << service;
@@ -1113,7 +1147,7 @@ void QLowEnergyControllerPrivateWinRT::discoverServiceDetails(
     }
 
     auto reactOnDiscoveryError = [](QSharedPointer<QLowEnergyServicePrivate> service,
-            const QString &msg)
+                                    const QString &msg)
     {
         qCDebug(QT_BT_WINDOWS) << msg;
         service->setError(QLowEnergyService::UnknownError);

src/bluetooth/qlowenergycontroller_winrt_p.h
@@ -159,6 +159,7 @@ private:
         EventRegistrationToken token;
     };
     QList<ValueChangedEntry> mValueChangedTokens;
+    QMap<QBluetoothUuid, Microsoft::WRL::ComPtr<ABI::Windows::Devices::Bluetooth::GenericAttributeProfile::IGattDeviceService>> m_openedServices;
 
     Microsoft::WRL::ComPtr<ABI::Windows::Devices::Bluetooth::GenericAttributeProfile::IGattDeviceService> getNativeService(const QBluetoothUuid &serviceUuid);
     Microsoft::WRL::ComPtr<ABI::Windows::Devices::Bluetooth::GenericAttributeProfile::IGattCharacteristic> getNativeCharacteristic(const QBluetoothUuid &serviceUuid, const QBluetoothUuid &charUuid);
@@ -180,6 +181,9 @@ private:
         Microsoft::WRL::ComPtr<ABI::Windows::Devices::Bluetooth::GenericAttributeProfile::IGattDeviceService> nativeService);
     HRESULT onServiceDiscoveryFinished(ABI::Windows::Foundation::IAsyncOperation<ABI::Windows::Devices::Bluetooth::GenericAttributeProfile::GattDeviceServicesResult *> *op,
                                        ABI::Windows::Foundation::AsyncStatus status);
+
+    void clearAllServices();
+    void closeAndRemoveService(const QBluetoothUuid &uuid);
 };
 
 QT_END_NAMESPACE

commit 4c92565f04755eb011be4b893d2619e25fb008c4

QLowEnergyController Windows: fix UI hangs during discovery on disconnected paired devices
Commit 0b60ca266f0fe27053a58eff3dbd903e3a1678ca introduced Close()
calls for GattDeviceService objects. IIRC, it was done to avoid
potential GattCommunicationStatus_AccessDenied errors, which could
otherwise happen when trying to re-acquire the non-closed service.
This commit does the proper cleanup when the device is disconnected.

However there is one corner-case: when we try to connect to a paired
device which is turned off, Windows provides its services (using some
cached data), but the service details discovery fails.
The details discovery is executed in a background thread using a set
of async calls. If the user disconnects from the device during this
discovery, the GattDeviceService::Close() call for the respective
service will block until the async operation fails (which takes
a couple of seconds on Windows 11). As a result, the UI thread freezes
for this time.

This patch is an attempt to avoid it by shifting the Close() call to
the background helper thread in such cases. If there is an error during
details discovery, or if it just takes too long, and the user decides
to disconnect from the device, the service will be closed directly
in the helper thread.
However, if the details discovery is completed successfully, the
GattDeviceService object can still be re-used for other operations,
and it will not be deleted from cache until the device is disconnected.

Fixes: QTBUG-108461
Pick-to: 6.5 6.4 6.2
Change-Id: I8ebe945130808ed7bd8852a76bba84c73651f5a5
Reviewed-by: Juha Vuolle <juha.vuolle@qt.io>

src/bluetooth/qlowenergycontroller_winrt.cpp
@@ -102,13 +102,14 @@ static QByteArray byteArrayFromGattResult(const ComPtr<IGattReadResult> &gattRes
     return byteArrayFromBuffer(buffer, isWCharString);
 }
 
-static void closeDeviceService(ComPtr<IGattDeviceService> service)
+template <typename T>
+static void closeDeviceService(ComPtr<T> service)
 {
     ComPtr<IClosable> closableService;
     HRESULT hr = service.As(&closableService);
-    RETURN_IF_FAILED("Could not cast service to closable", return);
+    RETURN_IF_FAILED("Could not cast type to closable", return);
     hr = closableService->Close();
-    RETURN_IF_FAILED("Service Close() failed", return);
+    RETURN_IF_FAILED("Close() call failed", return);
 }
 
 class QWinRTLowEnergyServiceHandler : public QObject
@@ -125,6 +126,8 @@ public:
 
     ~QWinRTLowEnergyServiceHandler()
     {
+        if (mAbortRequested)
+            closeDeviceService(mDeviceService);
         qCDebug(QT_BT_WINDOWS) << __FUNCTION__;
     }
 
@@ -323,7 +326,7 @@ public slots:
     }
 
 private:
-    bool checkAllCharacteristicsDiscovered();
+    void checkAllCharacteristicsDiscovered();
     void emitErrorAndQuitThread(HRESULT hr);
     void emitErrorAndQuitThread(const QString &error);
 
@@ -346,18 +349,13 @@ signals:
     void errorOccured(const QString &error);
 };
 
-bool QWinRTLowEnergyServiceHandler::checkAllCharacteristicsDiscovered()
+void QWinRTLowEnergyServiceHandler::checkAllCharacteristicsDiscovered()
 {
-    if (mCharacteristicsCountToBeDiscovered == 0) {
+    if (!mAbortRequested && (mCharacteristicsCountToBeDiscovered == 0)) {
         emit charListObtained(mService, mCharacteristicList, mIndicateChars,
                               mStartHandle, mEndHandle);
-        QThread::currentThread()->quit();
-        return true;
-    } else if (mAbortRequested) {
-        QThread::currentThread()->quit();
     }
-
-    return false;
+    QThread::currentThread()->quit();
 }
 
 void QWinRTLowEnergyServiceHandler::emitErrorAndQuitThread(HRESULT hr)
@@ -367,6 +365,7 @@ void QWinRTLowEnergyServiceHandler::emitErrorAndQuitThread(HRESULT hr)
 
 void QWinRTLowEnergyServiceHandler::emitErrorAndQuitThread(const QString &error)
 {
+    mAbortRequested = true; // so that the service is closed during cleanup
     emit errorOccured(error);
     QThread::currentThread()->quit();
 }
@@ -1158,6 +1157,12 @@ HRESULT QLowEnergyControllerPrivateWinRT::onServiceDiscoveryFinished(ABI::Window
 
 void QLowEnergyControllerPrivateWinRT::clearAllServices()
 {
+    // These services will be closed in the respective
+    // QWinRTLowEnergyServiceHandler workers (in background threads).
+    for (auto &uuid : m_requestDetailsServiceUuids)
+        m_openedServices.remove(uuid);
+    m_requestDetailsServiceUuids.clear();
+
     for (auto service : m_openedServices) {
         closeDeviceService(service);
     }
@@ -1295,6 +1300,7 @@ void QLowEnergyControllerPrivateWinRT::discoverServiceDetailsHelper(
 
     QWinRTLowEnergyServiceHandler *worker =
             new QWinRTLowEnergyServiceHandler(service, deviceService3, mode);
+    m_requestDetailsServiceUuids.insert(service);
     QThread *thread = new QThread;
     worker->moveToThread(thread);
     connect(thread, &QThread::started, worker, &QWinRTLowEnergyServiceHandler::obtainCharList);
@@ -1313,6 +1319,7 @@ void QLowEnergyControllerPrivateWinRT::discoverServiceDetailsHelper(
                     << "Discovery complete for unknown service:" << service.toString();
             return;
         }
+        m_requestDetailsServiceUuids.remove(service);
 
         QSharedPointer<QLowEnergyServicePrivate> pointer = serviceList.value(service);
         pointer->startHandle = startHandle;

src/bluetooth/qlowenergycontroller_winrt_p.h
@@ -126,6 +126,7 @@ private:
 
     using GattDeviceServiceComPtr = Microsoft::WRL::ComPtr<ABI::Windows::Devices::Bluetooth::GenericAttributeProfile::IGattDeviceService>;
     QMap<QBluetoothUuid, GattDeviceServiceComPtr> m_openedServices;
+    QSet<QBluetoothUuid> m_requestDetailsServiceUuids;
 
     using NativeServiceCallback = std::function<void(GattDeviceServiceComPtr)>;
     HRESULT getNativeService(const QBluetoothUuid &serviceUuid, NativeServiceCallback callback);