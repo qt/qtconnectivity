fbe1a265afef464c6aac71db77a50eda17e82f4e
Juha Vuolle <juha.vuolle@qt.io>
2023-03-31 14:15:21 +0300

    Add permission request to bttestui manual test application
    In addition add temporary workaround for linking against the permission
    plugin on macOS
    
    Task-number: QTBUG-112215
    Task-number: QTBUG-112212
    Change-Id: Idbd8c4cbdfc58893dc23147379265030cefc2aee
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>

--- tests/bttestui/main.cpp
+++ tests/bttestui/main.cpp
@@ -8,12 +8,35 @@
 
 #include <QtCore/QLoggingCategory>
 
+#if QT_CONFIG(permissions)
+#include <QtCore/qpermissions.h>
+#endif
+
 #include "btlocaldevice.h"
 
 int main(int argc, char *argv[])
 {
     QLoggingCategory::setFilterRules(QStringLiteral("qt.bluetooth* = true"));
     QGuiApplication app(argc, argv);
+
+#if QT_CONFIG(permissions)
+    // Check Bluetooth permission and request it if the app doesn't have it
+    auto permissionStatus = app.checkPermission(QBluetoothPermission{});
+    if (permissionStatus == Qt::PermissionStatus::Undetermined) {
+        app.requestPermission(QBluetoothPermission{},
+                              [&permissionStatus](const QPermission &permission) {
+            qApp->exit(); // Exit the permission request processing started below
+            permissionStatus = permission.status();
+        });
+        // Process permission request
+        app.exec();
+    }
+    if (permissionStatus == Qt::PermissionStatus::Denied) {
+        qWarning("Bluetooth permission denied, exiting");
+        return -1;
+    }
+#endif
+
     qmlRegisterType<BtLocalDevice>("Local", 6, 5, "BluetoothDevice");
 
     QQuickView view;
