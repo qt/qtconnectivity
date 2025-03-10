e86b418e5791901a455c99a27de806c0a44bba23
Ivan Solovev <ivan.solovev@qt.io>
2025-03-10 10:28:33 +0100

    BlueZ: rename leadvertisement1 -> leadvertisement1adaptor
    ... because that's the common naming pattern for all other D-Bus
    adaptors used in the module.
    
    Pick-to: 6.9 6.8 6.5
    Change-Id: I1f555b0109b2b1d6ed2697b9966e6636bfdde3e2
    Reviewed-by: Juha Vuolle <juha.vuolle@qt.io>

--- src/bluetooth/qleadvertiser_bluezdbus.cpp
+++ src/bluetooth/qleadvertiser_bluezdbus.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qleadvertiser_bluezdbus_p.h"
-#include "bluez/leadvertisement1_p.h"
+#include "bluez/leadvertisement1adaptor_p.h"
 #include "bluez/leadvertisingmanager1_p.h"
 #include "bluez/bluez5_helper_p.h"
 
