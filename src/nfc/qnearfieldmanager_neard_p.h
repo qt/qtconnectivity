f3ed3fd1fa3950f8dbb2ba4d6db43f5e18315547
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-09-10 10:33:08 +0300

    Revert "Update commercial SPDX-License-Identifier"
    This reverts commit 4865c716f958a16ff00605a35625d4b2c87e302a.
    
    The revert is needed for the Qt 6.8 LTS opensource releases.
    They cannot contain commercial SPDX license identifiers.
    
    Task-number: QTLTSOSS-264
    Change-Id: I75a0bbc70286fc9398ace398c8516577e65bd745
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/nfc/qnearfieldmanager_neard_p.h
+++ src/nfc/qnearfieldmanager_neard_p.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2016 BlackBerry Limited, Copyright (C) 2016 BasysKom GmbH
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #ifndef QNEARFIELDMANAGER_NEARD_H
 #define QNEARFIELDMANAGER_NEARD_H
--- src/nfc/qnearfieldtarget_neard_p.cpp
+++ src/nfc/qnearfieldtarget_neard_p.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2016 BlackBerry Limited, Copyright (C) 2016 BasysKom GmbH
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qnearfieldtarget_neard_p.h"
 
