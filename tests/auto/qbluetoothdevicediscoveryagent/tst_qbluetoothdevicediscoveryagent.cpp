44c271817dc01ab938b1dccadb077292a4f909f0
Assam Boudjelthia <assam.boudjelthia@qt.io>
2026-04-15 17:15:51 +0300

    Android: run tst_qbluetooth{device,service}discoveryagent on emulators
    Those two tests don't require user interaction and should
    be fine to run an emulator in CI.
    
    Pick-to: 6.11
    Change-Id: Ia1d940d41ce14a1dfda275381533af577e920777
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>

--- tests/auto/qbluetoothdevicediscoveryagent/tst_qbluetoothdevicediscoveryagent.cpp
+++ tests/auto/qbluetoothdevicediscoveryagent/tst_qbluetoothdevicediscoveryagent.cpp
@@ -159,9 +159,6 @@ void tst_QBluetoothDeviceDiscoveryAgent::deviceDiscoveryDebug(const QBluetoothDe
 
 void tst_QBluetoothDeviceDiscoveryAgent::tst_startStopDeviceDiscoveries()
 {
-    if (androidBluetoothEmulator())
-        QSKIP("Skipping test on Android 12+ emulator, CI can timeout waiting for user input");
-
     QBluetoothDeviceDiscoveryAgent discoveryAgent;
 
     QVERIFY(discoveryAgent.error() == discoveryAgent.NoError);
@@ -447,9 +444,6 @@ void tst_QBluetoothDeviceDiscoveryAgent::tst_discoveryTimeout()
 
 void tst_QBluetoothDeviceDiscoveryAgent::tst_discoveryMethods()
 {
-    if (androidBluetoothEmulator())
-        QSKIP("Skipping test on Android 12+ emulator, CI can timeout waiting for user input");
-
     const QBluetoothLocalDevice localDevice;
     if (localDevice.allDevices().size() != 1) {
         // On iOS it returns 0 but we still have working BT.
